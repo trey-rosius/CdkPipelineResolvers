42565e3534712f0b201c661d5ceaff56d9ddefd0
trey-rosius <treyrosius@gmail.com>
2022-06-04 14:47:11 +0100

    Initial commit

---
+++ test/cdk-pipeline-resolvers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipelineResolvers from '../lib/cdk-pipeline-resolvers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-resolvers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipelineResolvers.CdkPipelineResolversStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
